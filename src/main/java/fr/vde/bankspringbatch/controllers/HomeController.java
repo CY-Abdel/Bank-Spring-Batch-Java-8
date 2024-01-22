6925e428294e52a37329699d11ea7ea812752c4c
Abdelhadi Chougar <chougar.abdelhadi@gmail.com>
2024-01-22 09:54:38 +0100

    init projet spring batch and set pom.xml with java 8

---
+++ src/main/java/fr/vde/bankspringbatch/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package fr.vde.bankspringbatch.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+  @GetMapping("/")
+  public String home() {
+    return "index2"; //  vue  "index" dans templates
+  }
+}
